074d54f7edb27727352933f4d1f028e8a5a86106
Nfafshari <nathen.afshari@gmail.com>
2026-07-02 09:08:35 -0500

    Created initial files ready to be developed.

---
+++ src/app/(protected)/dashboard/budget/client.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function Budget () {
+  return (
+    <div className="flex flex-col w-full p-5 min-h-full">
+      
+    </div>
+  );
+}
---
+++ src/app/(protected)/dashboard/budget/page.tsx
@@ -0,0 +1,7 @@
+import Budget from "./client";
+
+export default function BudgetWrapper () {
+  return (
+    <Budget />
+  );
+}
